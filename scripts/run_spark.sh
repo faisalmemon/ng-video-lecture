bd275da984ba9b9021d01f802d8aa8427a891551
Faisal Memon <faisalmemonuk@yahoo.com>
2026-04-01 10:10:27 +0100

    Add a docker run script.

---
+++ scripts/run_spark.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# run_spark.sh
+
+docker run --gpus all -it --rm \
+  --user $(id -u):$(id -g) \
+  -v /home/faisalm/dev:/workspace \
+  -w /workspace/ng-video-lecture \
+  --ipc=host \
+  -e HOME=/tmp \
+  -p 6006:6006 \
+  nvcr.io/nvidia/pytorch:26.01-py3 \
+  /bin/bash
