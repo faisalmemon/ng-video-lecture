b709687485163fcc61cff001ee423d67ee0f4869
Faisal Memon <faisalmemonuk@yahoo.com>
2026-04-02 17:19:03 +0100

    Attention now gives us val loss of 2.4043.

--- fbigram.py
+++ fbigram.py
@@ -5,9 +5,9 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 8 # what is the maximum context length for predictions?
-max_iters = 3000
+max_iters = 5000
 eval_interval = 300
-learning_rate = 1e-2
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32
