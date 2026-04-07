a84d1c8267e7ede4f5e16e36eefc4924168fbe68
Faisal Memon <faisalmemonuk@yahoo.com>
2026-04-07 17:56:03 +0100

    Loss now 2.2643 via multi-head attention.

--- fbigram.py
+++ fbigram.py
@@ -67,7 +67,7 @@ class BigramLanguageModel(nn.Module):
         # n_embd is number of embedding dimensions
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)
+        self.sa_heads = MultiHeadAttention(4, n_embd//4)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -76,7 +76,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
         x = tok_emb + pos_emb # (B,T,C)
-        x = self.sa_head(x) # (B,T,C)
+        x = self.sa_heads(x) # (B,T,C)
         logits = self.lm_head(x) # (B,T,vocab_size)
 
 
@@ -131,6 +131,20 @@ class Head(nn.Module):
 
         # perform the weighted aggregation of the values
         out = wei @ v # (B,T,T) @ (B,T,head_size) -> (B,T,head_size)
+        return out
+    
+class MultiHeadAttention(nn.Module):
+
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, n_embd)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
         return out
 
 
