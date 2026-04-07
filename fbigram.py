737b696aa37a41e738909841f801d34720291af7
Faisal Memon <faisalmemonuk@yahoo.com>
2026-04-07 22:44:05 +0100

    Loss now 2.2556.

--- fbigram.py
+++ fbigram.py
@@ -58,6 +58,19 @@ def estimate_loss():
     model.train()
     return out
 
+class FeedForward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+    
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
@@ -68,6 +81,7 @@ class BigramLanguageModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
         self.sa_heads = MultiHeadAttention(4, n_embd//4)
+        self.ffwd = FeedForward(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -77,6 +91,7 @@ class BigramLanguageModel(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
         x = tok_emb + pos_emb # (B,T,C)
         x = self.sa_heads(x) # (B,T,C)
+        x = self.ffwd(x) # (B,T,C)
         logits = self.lm_head(x) # (B,T,vocab_size)
 
 
